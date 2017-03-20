9fc586ad5c9714ebe7c10b5e18a944483e523d83
Ryan Ngo <rawrimryno@gmail.com>
2017-03-20 00:53:39 -0500

    initial forms

--- js/TutorStudy.js
+++ js/TutorStudy.js
@@ -9,24 +9,47 @@ By: Ryan Ngo
 
 /* General Get Functions for data loading */
 function getMajors(){
-    $.ajax({
-        dataType: "json",
-        url: "http://52.38.218.199/TutorStudyServlet/GetMajors",
-        success:function(data){
-            $("#majorSelect").select2({
-                data:data
-            });
+    //note: needed 2 different methods because select2 does not allow multiple dropdown parents
+    $("#studentMajorSelect").select2({
+        ajax: {
+            url: "http://52.38.218.199/TutorStudyServlet/GetMajors",
+            processResults: function(data){
+                return{
+                    results: $.map(data, function(data){
+                        return{
+                            id: data.id,
+                            text: data.major         
+                        }
+                    })
+                }
+            }
+        },
+        dropdownParent: $("#studentRegistrationForm"),
+        createTag: function () {
+            // Disable tagging
+            return null;
         }
     });
-    /*$.ajax({
-        type: "GET",
-        url: "http://52.38.218.199/TutorStudyServlet/GetMajors",
-        contentType:"application/json",
-        success: function(data){
-            console.log(JSON.stringify(data));
-            return data;
+    $("#tutorMajorSelect").select2({
+        ajax: {
+            url: "http://52.38.218.199/TutorStudyServlet/GetMajors",
+            processResults: function(data){
+                return{
+                    results: $.map(data, function(data){
+                        return{
+                            id: data.id,
+                            text: data.major         
+                        }
+                    })
+                }
+            }
+        },
+        dropdownParent: $("#tutorRegistrationForm"),
+        createTag: function () {
+            // Disable tagging
+            return null;
         }
-    })*/
+    });
 }
 /* End General Get Functions */
 
@@ -36,6 +59,7 @@ function getMajors(){
 /*Form Functions*/
 function changeRegisterForm(type){
     var accountType = type.value;
+    getMajors();
     switch(accountType){
         case("Student"):
             $("#tutorRegistrationForm").hide();
@@ -45,7 +69,6 @@ function changeRegisterForm(type){
         case("Tutor"):
             $("#studentRegistrationForm").hide();
             $("#tutorRegistrationForm").show("slow");
-            getMajors();
             console.log("tutor form loaded");
             break;
         default:
@@ -53,3 +76,9 @@ function changeRegisterForm(type){
     }
 }
 /*End Form Functions */
+
+/*Register Functions*/
+function registerTutor(){
+    console.log($("#tutorRegistrationForm").serializeArray());
+}
+/* End Register Functions*/
