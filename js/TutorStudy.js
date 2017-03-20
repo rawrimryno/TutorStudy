7794758a70bdcbffaf464a2876b0cf5deae50c68
Ryan Ngo <rawrimryno@gmail.com>
2017-03-19 22:21:18 -0500

    getting major data

---
+++ js/TutorStudy.js
@@ -0,0 +1,58 @@
+/*
+
+This is a custom js for the TutorStudy website.
+
+By: Ryan Ngo
+
+*/
+
+
+/* General Get Functions for data loading */
+function getMajors(){
+    $.ajax({
+        dataType: "json",
+        url: "./TutorStudyServlet/GetMajors",
+        success:function(data){
+            console.log(JSON.stringify(data));
+            return data;
+        }
+    });
+    /*$.ajax({
+        type: "GET",
+        url: "http://52.38.218.199/TutorStudyServlet/GetMajors",
+        contentType:"application/json",
+        success: function(data){
+            console.log(JSON.stringify(data));
+            return data;
+        }
+    })*/
+}
+/* End General Get Functions */
+
+/* General Load Functions */
+/* End General Load Functions */
+
+/*Form Functions*/
+function changeRegisterForm(type){
+    var accountType = type.value;
+    switch(accountType){
+        case("Student"):
+            $("#tutorRegistrationForm").hide();
+            $("#studentRegistrationForm").show("slow");
+            console.log("student form loaded");
+            break;
+        case("Tutor"):
+            $("#studentRegistrationForm").hide();
+            $("#tutorRegistrationForm").show("slow");
+            var majorData = getMajors();
+            console.log(majorData);
+            $("#majorSelect").select2({
+
+            });
+            console.log("tutor form loaded");
+            break;
+        default:
+            console.log("Error: Account Type not found");
+    }
+}
+/*End Form Functions */
