c68131581145b2430aa4b675531c97a1d44b1fa7
Ryan Ngo <rawrimryno@gmail.com>
2017-04-10 17:02:28 -0500

    proper modal toggle

--- js/TutorStudy.js
+++ js/TutorStudy.js
@@ -158,7 +158,7 @@ function Login(){
                 $("#alertLoginInfo").text(data);
                 $("#loginInfo").show('fast');
                 loadUser();
-                $("#loginModal").close();
+                $("#loginModal").modal('toggle');
             }
         });
     }
