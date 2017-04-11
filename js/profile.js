8e4a9e24826e913444232352890dbb71d05af5f3
Ryan Ngo <rawrimryno@gmail.com>
2017-04-11 10:27:52 -0500

    asdf

--- js/profile.js
+++ js/profile.js
@@ -90,7 +90,7 @@ function updateTutor(){
         data: fields,
         success: function(data){
             $("#userInfo").removeClass("alert-success");
-            $("#userInfo").addClass("alert-infoS");
+            $("#userInfo").addClass("alert-info");
             $("#alertUserInfo").text(data);
             $("#userInfo").show('fast');
             setTimeout(function(){$("#userInfo").hide('fast');}, 2000);
