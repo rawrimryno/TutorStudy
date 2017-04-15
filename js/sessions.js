a40e3bdcdb7762ff557b06b33ab36da3de5e2d15
Ryan Ngo <rawrimryno@gmail.com>
2017-04-15 10:43:33 -0500

    check login for sessions

---
+++ js/sessions.js
@@ -0,0 +1,17 @@
+/*
+
+This is a custom js for the TutorStudy website.
+
+By: Ryan Ngo
+
+*/
+
+/* Generic Functions */
+/* End Generic Functions */
+
+/* Document Ready Functions */
+$(document).ready(function(){
+    checkLogin();
+}
+
+/* End Document Ready Functions */
