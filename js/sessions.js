bae7184015348e4e30528a4e78b39884d88696a7
Ryan Ngo <rawrimryno@gmail.com>
2017-04-15 20:35:05 -0500

    changes

--- js/sessions.js
+++ js/sessions.js
@@ -35,6 +35,14 @@ function loadUserSession(){
     });
 }
 
+function changePendingSession(TSID, value){
+    if (value == 0){
+        $("#"+TSID).show('slow');
+    }else{
+        $("#"+TSID).hide('slow');
+    }
+}
+
 /* End Generic Functions */
 
 /* Load Session Functions */
@@ -56,12 +64,19 @@ function loadTutorSessions(){
                         Course: data.course,
                         Date: data.datetime,
                         Location: data.location,
-                        Action: "<select class='form-control'>"+
+                        Action: "<select class='form-control' onChange='changePendingSession("+data.TSID+",this.value);'>"+
                                     "<option value='1'>Accept</option>"+
                                     "<option value='0'>Reschedule</option>"+
                                     "<option value='-1'>Decline</option>"+
-                                "</select><br>"+
-                                "<button class='btn btn-primary'>Submit Response</button>"
+                                "</select>"+
+                                "<form id='"+data.TSID+"' class='form-control' style='display:none'>"+
+                                    "<label for='date'>Choose an available date:</label>"+
+                                    "<input name = 'date'class='form-control' type='text' id='datepicker'>"+
+                                    "<label for='time'>Choose an available time:</label>"+
+                                    "<input name = 'time'class='form-control' type='text' id='timepicker'>"+
+                                    "<label for='location'>Please input a location:</label>"+
+                                    "<input name = 'location'class='form-control' type='text' id='location' value='"+data.location+"'>"+
+                                "<button cass='btn btn-primary'>Submit Response</button>"
                     };
                 });
         var dynatable = $('#pendingTutorSessions').dynatable({
