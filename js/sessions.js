890150681bf84d8c782f57158e6075726fef7489
Ryan Ngo <rawrimryno@gmail.com>
2017-04-15 19:02:10 -0500

    adding tutor sessions

--- js/sessions.js
+++ js/sessions.js
@@ -7,11 +7,33 @@ By: Ryan Ngo
 */
 
 /* Generic Functions */
+function loadUserSession(){
+    $.ajax({
+        type: "GET",
+        async: false,
+        dataType: "JSON",
+        url: "http://52.38.218.199/TutorStudyServlet/GetUserInfo",
+        success: function(data){
+            var result = data;
+            switch(result.UTID){
+                case 2:
+                    break;
+                case 3:
+                    $("#tutorSessions").show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+}
+
 /* End Generic Functions */
 
 /* Document Ready Functions */
 $(document).ready(function(){
-    //checkLogin();
+    checkLogin();
+    loadUserSession();
 });
 
 /* End Document Ready Functions */
